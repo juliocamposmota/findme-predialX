fcc7d159064b5513c071d1401634986bf980a4e9
Júlio Mota <juliomotacampos@gmail.com>
2021-11-01 21:22:03 -0300

    cria rota de busca por nome e instala a dependencia body parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ const clients = [
 ];
 
 app.get('/clients', getClients);
+app.get('/clients/search', searchClients);
 app.get('/clients/:id', getClient);
 
 app.listen(3001, () => {
@@ -29,4 +30,10 @@ function getClient(req, res) {
   if (!recipe) return res.status(404).json({ message: 'Client not found' });
 
   res.status(200).json(recipe);
+}
+
+function searchClients(req, res) {
+  const { name } = req.query;
+  const filtredClients = clients.filter((c) => c.name.includes(name));
+  res.status(200).json(filtredClients);
 }
