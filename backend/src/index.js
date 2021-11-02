dcbde81e152d88f72056111194a99e0137f90389
Júlio Mota <juliomotacampos@gmail.com>
2021-11-01 21:11:21 -0300

    implementa as rotas clients e clients/:id

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const cors = require('cors'); // permitir outras aplicacoes consumirem esta api.
 
 const app = express();
 
-app.get('/hello', handleHelloWorldRequest);
+app.use(cors());
+
+const clients = [
+  { id: 1, name: 'PredialX' },
+  { id: 2, name: 'XPTO ManPred'},
+  { id: 3, name: 'Pred Service' },
+];
+
+app.get('/clients', getClients);
+app.get('/clients/:id', getClient);
 
 app.listen(3001, () => {
   console.log('aplicação escutando a porta 3001');
 });
 
-function handleHelloWorldRequest(req, res) {
-  res.status(200).send('Hello World! Com nodemon');
+function getClients(req, res) {
+  res.json(clients);
+}
+
+function getClient(req, res) {
+  const { id } = req.params;
+  const recipe = clients.find((c) => c.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Client not found' });
+
+  res.status(200).json(recipe);
 }
