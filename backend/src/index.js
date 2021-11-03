7a56d260f2a4c24f1bdb81fdbfd48fb193cff2af
Júlio Mota <juliomotacampos@gmail.com>
2021-11-03 17:31:18 -0300

    ajusta porta do back end

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const orderRouter = require('./routes/Order');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
