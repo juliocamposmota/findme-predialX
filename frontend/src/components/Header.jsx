e39bdff3725d4992b31b911d2a8b9b6dcdb6e412
Júlio Mota <juliomotacampos@gmail.com>
2021-11-03 22:04:15 -0300

    estiliza header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import './styles/Header.css';
 
 function Header() {
   return (
-    <header>
-      <section>
-        <p>PredialX</p>
-      </section>
+    <header className="header">
+      <nav className="nav">
+        <div>
+          <p className="logo">PredialX</p>
+        </div>
+
+        <div>
+          <ul className="list">
+            <li className="item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="item">
+              <Link to="/">Clientes</Link>
+            </li>
+            <li className="item">
+              <Link to="/">Ordens</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </header>
   );
 }
